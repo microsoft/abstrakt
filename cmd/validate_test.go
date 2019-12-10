a7dbcd360b94d3fd69de896837f79d264dfbb99a
Jason <jagoodse@microsoft.com>
2019-12-10 17:04:24 +1100

    Adding validate command unit test.

---
+++ cmd/validate_test.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestValidateCommand(t *testing.T) {
+	expected := "open does-not-exist: no such file or directory"
+
+	constellationPath, _, _ := PrepareRealFilesForTest(t)
+	output, err := executeCommand(newValidateCmd().cmd, "-f", constellationPath)
+
+	if err != nil {
+		t.Errorf("Did not received expected error. \nGot:\n %v", output)
+	}
+
+	output, err = executeCommand(newValidateCmd().cmd, "-f", "does-not-exist")
+
+	if err != nil {
+		checkStringContains(t, err.Error(), expected)
+	} else {
+		t.Errorf("Did not received expected error. \nExpected: %v\nGot:\n %v", expected, err.Error())
+	}
+}
