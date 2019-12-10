6639d75ecac23b243d0edc9cc0f3916d171177f8
Jason <jagoodse@microsoft.com>
2019-12-10 13:41:34 +1100

    feat: updating validation command to utilise validation service.

--- cmd/validate.go
+++ cmd/validate.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/microsoft/abstrakt/internal/dagconfigservice"
 	"github.com/microsoft/abstrakt/internal/tools/logger"
+	"github.com/microsoft/abstrakt/internal/validationservice"
 	"github.com/spf13/cobra"
 )
 
@@ -19,11 +22,46 @@ func newValidateCmd() *validateCmd {
 		Long: `Validate is used to ensure the correctness of a constellation file.
 	
 Example: abstrakt validate -f [constellationFilePath]`,
+		SilenceUsage:  true,
+		SilenceErrors: true,
 
-		RunE: func(cmd *cobra.Command, args []string) error {
-			logger.Info(cc.constellationFilePath)
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			d := dagconfigservice.NewDagConfigService()
+			err = d.LoadDagConfigFromFile(cc.constellationFilePath)
 
-			return nil
+			if err != nil {
+				return err
+			}
+
+			service := validationservice.Validator{Config: &d}
+
+			valid := service.CheckDuplicates()
+
+			if len(valid) > 0 {
+				logger.Error("Duplicate IDs found:")
+				for _, i := range valid {
+					logger.Error(i)
+				}
+				err = error(fmt.Errorf("Constellation is invalid"))
+			}
+
+			connections := service.CheckServiceExists()
+
+			if len(connections) > 0 {
+				for key, i := range connections {
+					logger.Errorf("Relationship '%v' has missing Services:", key)
+					for _, j := range i {
+						logger.Error(j)
+					}
+				}
+				err = error(fmt.Errorf("Constellation is invalid"))
+			}
+
+			if err == nil {
+				logger.Info("Constellation is valid.")
+			}
+
+			return err
 		},
 	})
 
