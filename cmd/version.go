b31623925711573459258ef0b73f8d3e7301910b
Jason <jagoodse@microsoft.com>
2019-12-11 19:43:34 +1100

    Updated PrintVersion in version.go to return abstrakt version.

--- cmd/version.go
+++ cmd/version.go
@@ -17,14 +17,14 @@ func newVersionCmd() *versionCmd {
 		Short: "The version of Abstrakt being used",
 		Long:  "The version of Abstrakt being used",
 		Run: func(cmd *cobra.Command, args []string) {
-			PrintVersion()
+			logger.Infof("abstrakt version %v", Version())
 		},
 	})
 
 	return cc
 }
 
-// PrintVersion prints the current version of Abstrakt being used.
-func PrintVersion() {
-	logger.Info("abstrakt version 0.0.1")
+// Version returns the version of abstrakt running.
+func Version() string {
+	return "0.0.1"
 }
