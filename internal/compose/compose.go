0e2fbbce808b3b9f1167a3d9beb1e33eb44d06c1
Jason <jagoodse@microsoft.com>
2020-01-23 15:05:18 +1100

    Cleanup compose.

--- internal/compose/compose.go
+++ internal/compose/compose.go
@@ -15,8 +15,8 @@ type Composer struct {
 }
 
 //Build takes the loaded DAG and maps and builds the Helm values and requirements documents
-func (m *Composer) Build(name string, dir string) (*helm.Chart, error) {
-	if m.Constellation.Name == "" || m.Mapper.Name == "" {
+func (c *Composer) Build(name string, dir string) (*helm.Chart, error) {
+	if c.Constellation.Name == "" || c.Mapper.Name == "" {
 		return nil, fmt.Errorf("Please initialize with LoadFromFile or LoadFromString")
 	}
 
@@ -32,8 +32,8 @@ func (m *Composer) Build(name string, dir string) (*helm.Chart, error) {
 
 	values := newChart.Values
 
-	for _, n := range m.Constellation.Services {
-		service := m.Mapper.FindByType(n.Type)
+	for _, n := range c.Constellation.Services {
+		service := c.Mapper.FindByType(n.Type)
 		if service == nil {
 			return nil, fmt.Errorf("Could not find service %v", service)
 		}
@@ -66,15 +66,15 @@ func (m *Composer) Build(name string, dir string) (*helm.Chart, error) {
 
 		relationships := make(map[string][]interface{})
 		valMap["relationships"] = &relationships
-		toRels := m.Constellation.FindRelationshipByToName(n.ID)
-		fromRels := m.Constellation.FindRelationshipByFromName(n.ID)
+		toRels := c.Constellation.FindRelationshipByToName(n.ID)
+		fromRels := c.Constellation.FindRelationshipByFromName(n.ID)
 
 		for _, i := range toRels {
 			toRelations := make(map[string]string)
 			relationships["input"] = append(relationships["input"], &toRelations)
 
 			//find the target service
-			foundService := m.Constellation.FindService(i.From)
+			foundService := c.Constellation.FindService(i.From)
 
 			if foundService == nil {
 				return nil, fmt.Errorf("Service '%v' referenced in relationship '%v' not found", i.From, i.ID)
@@ -96,7 +96,7 @@ func (m *Composer) Build(name string, dir string) (*helm.Chart, error) {
 			relationships["output"] = append(relationships["output"], &fromRelations)
 
 			//find the target service
-			foundService := m.Constellation.FindService(i.To)
+			foundService := c.Constellation.FindService(i.To)
 
 			if foundService == nil {
 				return nil, fmt.Errorf("Service '%v' referenced in relationship '%v' not found", i.To, i.ID)
@@ -121,19 +121,10 @@ func (m *Composer) Build(name string, dir string) (*helm.Chart, error) {
 }
 
 //LoadFile takes a string dag and map and loads them
-func (m *Composer) LoadFile(dagFile string, mapFile string) (err error) {
-	err = m.Constellation.LoadFile(dagFile)
+func (c *Composer) LoadFile(dagFile string, mapFile string) (err error) {
+	err = c.Constellation.LoadFile(dagFile)
 	if err != nil {
 		return err
 	}
-	return m.Mapper.LoadFile(mapFile)
-}
-
-//LoadString takes a string dag and map and loads them
-func (m *Composer) LoadString(dagString string, mapString string) (err error) {
-	err = m.Constellation.LoadString(dagString)
-	if err != nil {
-		return
-	}
-	return m.Mapper.LoadString(mapString)
+	return c.Mapper.LoadFile(mapFile)
 }
--- internal/compose/compose_test.go
+++ internal/compose/compose_test.go
@@ -31,7 +31,10 @@ func TestComposeService(t *testing.T) {
 
 	assert.Error(t, err, "Compose should fail if not yet loaded")
 
-	_ = comp.LoadString(test01DagStr, configMapTest01String)
+	dag := "testdata/constellation.yaml"
+	mapper := "testdata/mapper.yaml"
+
+	_ = comp.LoadFile(dag, mapper)
 
 	h, err := comp.Build("test", tdir)
 
@@ -109,70 +112,15 @@ func TestHelmLibCompose(t *testing.T) {
 func TestLoadFromString(t *testing.T) {
 	comp := new(compose.Composer)
 
-	err := comp.LoadString(test01DagStr, configMapTest01String)
+	dag := "testdata/constellation.yaml"
+	mapper := "testdata/mapper.yaml"
+
+	err := comp.LoadFile(dag, mapper)
 	assert.NoErrorf(t, err, "Error: %v", err)
 
-	err = comp.LoadString("sfdsd", configMapTest01String)
+	err = comp.LoadFile("sfdsd", mapper)
 	assert.Error(t, err, "Didn't get error when should")
 
-	err = comp.LoadString(test01DagStr, "sdfsdf")
+	err = comp.LoadFile(dag, "sdfsdf")
 	assert.Error(t, err, "Didn't get error when should")
 }
-
-const test01DagStr = `Name: "Azure Event Hubs Sample"
-Id: "d6e4a5e9-696a-4626-ba7a-534d6ff450a5"
-Services:
-- Name: "Event Generator"
-  Id: "9e1bcb3d-ff58-41d4-8779-f71e7b8800f8"
-  Type: "EventGenerator"
-  Properties: {}
-- Name: "Azure Event Hub"
-  Id: "3aa1e546-1ed5-4d67-a59c-be0d5905b490"
-  Type: "EventHub"
-  Properties: {}
-- Name: "Event Logger"
-  Id: "a268fae5-2a82-4a3e-ada7-a52eeb7019ac"
-  Type: "EventLogger"
-  Properties: {}
-- Name: "Event Logger"
-  Id: "1d0255d4-5b8c-4a52-b0bb-ac024cda37e5"
-  Type: "EventLogger"
-  Properties: {}
-Relationships:
-- Name: "Generator to Event Hubs Link"
-  Id: "211a55bd-5d92-446c-8be8-190f8f0e623e"
-  Description: "Event Generator to Event Hub connection"
-  From: "9e1bcb3d-ff58-41d4-8779-f71e7b8800f8"
-  To: "3aa1e546-1ed5-4d67-a59c-be0d5905b490"
-  Properties: {}
-- Name: "Event Hubs to Event Logger Link"
-  Id: "08ccbd67-456f-4349-854a-4e6959e5017b"
-  Description: "Event Hubs to Event Logger connection"
-  From: "3aa1e546-1ed5-4d67-a59c-be0d5905b490"
-  To: "1d0255d4-5b8c-4a52-b0bb-ac024cda37e5"
-  Properties: {}
-- Name: "Event Hubs to Event Logger Link Repeat"
-  Id: "c8a719e0-164d-408f-9ed1-06e08dc5abbe"
-  Description: "Event Hubs to Event Logger connection"
-  From: "3aa1e546-1ed5-4d67-a59c-be0d5905b490"
-  To: "a268fae5-2a82-4a3e-ada7-a52eeb7019ac"
-  Properties: {}
-`
-
-const configMapTest01String = `
-Name: "Basic Azure Event Hubs maps"
-Id: "a5a7c413-a020-44a2-bd23-1941adb7ad58"
-Maps:
-- ChartName: "event_hub_sample_event_generator"
-  Type: "EventGenerator"
-  Location: "../../helm/basictest"
-  Version: "1.0.0"
-- ChartName: "event_hub_sample_event_logger"
-  Type: "EventLogger"
-  Location: "../../helm/basictest"
-  Version: "1.0.0"
-- ChartName: "event_hub_sample_event_hub"
-  Type: "EventHub"
-  Location: "../../helm/basictest"
-  Version: "1.0.0"
-`
